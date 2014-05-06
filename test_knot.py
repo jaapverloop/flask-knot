73754801ef0940bb3c185c1baf68c44d6fc2ede5
Jaap Verloop <j.verloop@gmail.com>
2014-05-06 23:43:52 +0200

    Add test for current_container.

--- test_knot.py
+++ test_knot.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import with_statement
+
 import unittest
 from flask import Flask
-from flask.ext.knot import Knot, get_container
+from flask.ext.knot import Knot, get_container, current_container
 
 
 def create_app():
@@ -55,6 +57,21 @@ class TestKnot(unittest.TestCase):
         app = create_app()
 
         self.assertRaises(RuntimeError, lambda: get_container(app))
+
+    def test_top_of_app_ctx_is_utilized_by_current_container(self):
+        app1 = create_app()
+        dic1 = Knot(app1)
+        dic1['foo'] = 'bar'
+
+        with app1.test_request_context():
+            self.assertEqual(current_container['foo'], 'bar')
+
+        app2 = create_app()
+        dic2 = Knot(app2)
+        dic2['foo'] = 'baz'
+
+        with app2.test_request_context():
+            self.assertEqual(current_container['foo'], 'baz')
 
 
 if __name__ == '__main__':
